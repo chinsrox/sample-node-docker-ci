6c5053f79d251bbda88c59750a89af27caf85356
Chinmay Tripathi <chinmay.tripathi@mheducation.com>
2016-10-24 16:09:32 -0400

    added circle.yml

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
-docker build -t willrstern/sample-node .
-docker push willrstern/sample-node
 
-ssh deploy@159.203.127.59 << EOF
-docker pull willrstern/sample-node:latest
+#!/bin/bash
+docker build -t chins/sample-node .
+docker push chins/sample-node
+
+ssh -i ~/.ssh/docker-chinmay.pem deploy@54.210.59.122 << EOF
+docker pull chins/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi willrstern/sample-node:current || true
-docker tag willrstern/sample-node:latest willrstern/sample-node:current
-docker run -d --net app --restart always --name web -p 3000:3000 willrstern/sample-node:current
+docker rmi chins/sample-node:current || true
+docker tag chins/sample-node:latest chins/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 chins/sample-node:current
 EOF
+
