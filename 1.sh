3d6a57f96121e9d0998a0504aa2aedab569abe30
Chinmay Tripathi <chinmay.tripathi@mheducation.com>
2016-10-24 15:36:05 -0400

    first commit

---
+++ 1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker build -t chins/sample-node .
+docker push chins/sample-node
+
+ssh -i ~/.ssh/docker-chinmay.pem deploy@54.210.59.122 << EOF
+docker pull chins/sample-node:latest
+docker stop web || true
+docker rm web || true
+docker rmi chins/sample-node:current || true
+docker tag chins/sample-node:latest chins/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 chins/sample-node:current
+EOF
+
